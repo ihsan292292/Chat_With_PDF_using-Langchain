d6ea61676c1ed974ef7aa09b22890b950023d2c3
Ihsan <97184876+ihsan292292@users.noreply.github.com>
2023-08-17 16:32:08 +0530

    completed

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+import pickle
+
 
 with st.sidebar:
     st.title("LLM Chat App")
@@ -10,6 +22,57 @@ with st.sidebar:
                 - [OpenAI](https://platform.openai.com/docs/models) LLM model
  
                 ''')
-    st.add_vertical_space(5)
-    st.write('Made with ❤️ by [Ihsan](https://youtube.com/@engineerprompt)')
+    add_vertical_space(5)
+    st.write('Made by [Ihsan](https://github.com/ihsan292292/Chat_With_PDF_using-Langchain)')
+    
+load_dotenv()
+
+def main():
+    st.header("Chat with PDF 💬")
+    pdf = st.file_uploader("Upload Your File", type='pdf')
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text=""
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+        
+        # text split
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+        # st.write(chunks)
+        
+        # embeddings
+        store_name = pdf.name[:-4]
+        st.write(f'{store_name}')
+        
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl","rb") as f:
+                VectorStore = pickle.load(f)
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks,embedding=embeddings)
+            with open(f"{store_name}.pkl","wb") as f:
+                pickle.dump(VectorStore, f)
+                
+        # Accept user questions/query 
+        query = st.text_input("Ask question about you PDF file:")
+        
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=2)
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm,chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+    
+if __name__ == '__main__':
+    main()
+    
+                
                 
